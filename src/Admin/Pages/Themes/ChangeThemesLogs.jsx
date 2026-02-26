f80ab9f5d7d212e59355b4f38ea013ce6d628577
Mohammad Tasin <miniofficial51@gmail.com>
2026-02-26 11:40:54 +0600

    Delete src directory

--- src/Admin/Pages/Themes/ChangeThemesLogs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ChangeThemesLogs = () => {
-  return (
-    <div>ChangeThemesLogs</div>
-  )
-}
-
-export default ChangeThemesLogs
--- src/Admin/Pages/Themes/GlobalThemeChange.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GlobalThemeChange = () => {
-  return (
-    <div>GlobalThemeChange</div>
-  )
-}
-
-export default GlobalThemeChange
