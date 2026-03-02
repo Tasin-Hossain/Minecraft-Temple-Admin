026bfb458420503b05a1fcf6a7af7ac2b0758a5c
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-02 16:43:20 +0600

    Env config add

---
+++ Server/src/config/env.js
@@ -0,0 +1,21 @@
+import '../validators/envValidators.js'; 
+
+export const config = {
+  NODE_ENV: process.env.NODE_ENV ,
+  PORT: Number(process.env.PORT) ,
+
+  MONGO_URI: process.env.MONGO_URI,
+
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN ,
+
+  EMAIL_SERVICE: process.env.EMAIL_SERVICE,
+  EMAIL_USER: process.env.EMAIL_USER,
+  EMAIL_PASS: process.env.EMAIL_PASS,
+
+  APP_URL: process.env.APP_URL,
+
+  RATE_LIMIT_WINDOW_MS: Number(process.env.RATE_LIMIT_WINDOW_MS) || 900000, 
+  RATE_LIMIT_MAX: Number(process.env.RATE_LIMIT_MAX) || 100,
+};
+
