2d41c7c120a91b86f5aadb6819c59dd7eb24732a
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-03 13:45:26 +0600

    update: server.js

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,26 +1,26 @@
 import { validateEnv } from "./validators/envValidators.js";
 import { config } from "./config/env.js";
-import app from './app.js';
+import app from "./app.js";
 import connectDB from "./config/db.js";
-
-
 
 // ENV validate
 validateEnv();
 
-const startSever = async()=>{
+const startSever = async () => {
   try {
 
     // mongodb
     await connectDB();
-
-    app.listen(config.PORT, ()=>{
-      console.log(`🚀 Server running in ${config.NODE_ENV} mode on port ${config.PORT}`);
-    })
+    
+    app.listen(config.PORT, () => {
+      console.log(
+        `🚀 Server running in ${config.NODE_ENV} mode on port ${config.PORT}`,
+      );
+    });
   } catch (error) {
-    console.error('❌ Startup failed:', error.message);
+    console.error("❌ Startup failed:", error.message);
     process.exit(1);
   }
-}
+};
 
 startSever();
