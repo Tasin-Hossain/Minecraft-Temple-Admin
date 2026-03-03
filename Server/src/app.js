dd9db4d8a8b0210301a500cf6c0cacef881a5286
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-03 15:36:54 +0600

    Update: add errorHandler

--- Server/src/app.js
+++ Server/src/app.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import errorHandler from './utils/erroeHandler.js';
 const app = express();
 
+app.use(express.json());
 
 app.get("/",(req, res)=>{
   res.json({
     ok: true
   })
 })
+
+// erro handler
+app.use(errorHandler);
 
 export default app;
