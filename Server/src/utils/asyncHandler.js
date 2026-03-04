a85bfc2a60049fe1b38682b271a11b75854850db
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-04 13:23:23 +0600

    Update(Utils): asyncHandle

--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,4 +1,6 @@
 //(async/await error wrapper)
 export const asyncHandler = (fn) => (req, res, next) => {
+  console.log("asyncHandler called");
   Promise.resolve(fn(req, res, next)).catch(next);
+  console.log("asyncHandler finished");
 };
