f282363e6b95960c9bf1bb82e712a9023a9e120a
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-05 13:51:09 +0600

    Utils: remove consol.log

--- Server/src/utils/asyncHandler.js
+++ Server/src/utils/asyncHandler.js
@@ -1,6 +1,4 @@
 //(async/await error wrapper)
 export const asyncHandler = (fn) => (req, res, next) => {
-  console.log("asyncHandler called");
   Promise.resolve(fn(req, res, next)).catch(next);
-  console.log("asyncHandler finished");
 };
