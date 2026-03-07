96cd8a0912df376a98767c60cea4d489f0ffc65c
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-07 15:10:24 +0600

    Update:  main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,11 +3,20 @@ import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./Admin/Components/Themes/ThemeContext.jsx";
 import { AdminThemeProvider } from "./Admin/Components/Themes/AdminThemeContext.jsx";
+import { store, persistor } from "./Redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
-  <ThemeProvider>
-    <AdminThemeProvider>
-      <App />
-    </AdminThemeProvider>
-  </ThemeProvider>
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading app...</div>} persistor={persistor}>
+      <ThemeProvider>
+        <AdminThemeProvider>
+          <App />
+          <ToastContainer autoClose={4000} hideProgressBar={true} toastClassName="toast" closeButton={false}/>
+        </AdminThemeProvider>
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>,
 );
