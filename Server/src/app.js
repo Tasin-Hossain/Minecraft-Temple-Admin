0295553c8d1b995f5fc81b4cd40f3423fce524be
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-08 15:04:30 +0600

    Update: add cors form app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -3,16 +3,28 @@ import errorHandler from "./middlewares/errorHandler.js";
 import authRouter from "./routes/auth.route.js";
 import AppError from "./utils/AppError.js";
 import { config } from "./config/env.js";
-
+import cors from "cors";
 const app = express();
 
-// Primary CORS
-app.use(
-  cors({
-    origin: config.FRONTEND_URL,
-    credentials: true,
-  })
-);
+const allowedOrigins = [
+  config.FRONTEND_URL,    
+  config.APP_URL,    
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,     
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(express.json());       
 app.use(express.urlencoded({ extended: true }));
