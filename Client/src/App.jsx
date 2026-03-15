53b024fdaf95d3e7d848eb6231df64469bb8c5d6
Tasin-Hossain <miniofficial51@gmail.com>
2026-03-15 12:52:44 +0600

    Update(App.jsx): add Breadcrumbs

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -17,6 +17,13 @@ import AllUsers from "./Admin/Pages/Members/AllMembers";
 import AllMembers from "./Admin/Pages/Members/AllMembers";
 import MembersDetails from "./Admin/Pages/Members/MembersDetails";
 import EditMember from "./Admin/Pages/Members/EditMember";
+import ApiLogs from "./Admin/Pages/Records/ApiLogs";
+import ActivityLogs from "./Admin/Pages/Records/ActivityLogs";
+import AuthLogs from "./Admin/Pages/Records/AuthLogs";
+import AdminLogs from "./Admin/Pages/Records/AdminLogs";
+import SecurityLogs from "./Admin/Pages/Records/SecurityLogs";
+import SystemLogs from "./Admin/Pages/Records/SystemLogs";
+import Breadcrumbs from "./Components/Breadcrumbs";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -29,43 +36,42 @@ const App = () => {
           index: true,
           element: <Dashboard />,
         },
-      
+
+        // Dashboard
         { path: "dashboard", element: <Dashboard /> },
 
-        { path: "/admin/users", element: <AllMembers /> },
-        { path: "/admin/users/:userId", element: <MembersDetails /> },
-        { path: "/admin/users/:userId/edit", element: <EditMember /> },
+        // Users
+        { path: "/admin/users", element: <div><Breadcrumbs /> <AllMembers /></div> },
+        { path: "/admin/users/:userId",element: <div><Breadcrumbs /> <MembersDetails /></div>},
+        { path: "/admin/users/:userId/edit",element: <div><Breadcrumbs /> <EditMember /></div>},
+
+        // Records
+        { path: "/admin/activity-logs", element: <div><Breadcrumbs /> <ActivityLogs /></div> },
+        { path: "/admin/api-logs", element: <div> <Breadcrumbs /> <ApiLogs /></div> },
+        { path: "/admin/auth-logs", element: <div><Breadcrumbs />  <AuthLogs /> </div>},
+        { path: "/admin/admin-logs", element: <div><Breadcrumbs />  <AdminLogs /></div> },
+        { path: "/admin/security-logs", element: <div><Breadcrumbs /> <SecurityLogs /></div> },
+        { path: "/admin/system-logs", element:<div><Breadcrumbs />  <SystemLogs /></div> },
 
         { path: "/admin/components", element: <Components /> },
         { path: "/admin/admin-theme-change", element: <AdminThemeChange /> },
         { path: "/admin/global-theme-change", element: <GlobalThemeChange /> },
         { path: "/admin/theme-logs", element: <ChangeThemesLogs /> },
-        
+
         // Profile
         { path: "/admin/settings", element: <Settings /> },
       ],
     },
+
+    
     // Auth
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/verify",
-      element: <Verify />,
-    },
-    {
-      path: "/2fa/verify",
-      element: <TwoFactorVerify />,
-    },
-    {
-      path: "/verify-email",
-      element: <VerifyEmail />,
-    },
+    { path: "/login",element: <Login />},
+    { path: "/register",element: <Register />},
+    { path: "/verify",element: <Verify />},
+    { path: "/2fa/verify",element: <TwoFactorVerify />},
+    { path: "/verify-email",element: <VerifyEmail />},
+
+
   ]);
   return (
     <>
